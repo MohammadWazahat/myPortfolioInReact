e3546589c09bdf62393bd3340f10dcca37dbcbcf
MohammadWazahat <muhammadwazahatrza092@gmail.com>
2024-06-02 18:09:26 +0530

    successfully added routing to my portfolio project

---
+++ src/components/myResume/MyResume.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyResume = () => {
+  return (
+    <div>
+      i am resume
+    </div>
+  )
+}
+
+export default MyResume
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,13 +3,47 @@ import Navbar from "../header/Navbar";
 import "./Portfolio.css";
 import Home from "../home/Home";
 import Footer from "../footer/Footer";
+import MyResume from "../myResume/MyResume"
+import MyProjects from "../myProjects/MyProjects"
+import AboutMe from "../aboutMe/AboutMe"
+import ContactMe from "../contactMe/ContactMe"
+
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
 
 const Portfolio = () => {
+    const router = createBrowserRouter([
+        {
+          path: "/home",
+          element: <><Navbar /><Home /><Footer /></>,
+        },
+        {
+            path: "/myResume",
+            element: <><Navbar /><MyResume/><Footer /></>,
+          },
+          {
+            path: "/myProjects",
+            element: <><Navbar /><MyProjects/><Footer /></>,
+          },
+          {
+            path: "/aboutMe",
+            element: <><Navbar /><AboutMe/><Footer /></>,
+          },
+          {
+            path: "/contactMe",
+            element: <><Navbar /><ContactMe/><Footer /></>,
+          },
+      ]);
   return (
     <div>
-      <Navbar />
+      {/* <Navbar />
       <Home />
-      <Footer/>
+      <Footer/> */}
+      
+      <RouterProvider router={router} >
+      </RouterProvider>
     </div>
   );
 };
